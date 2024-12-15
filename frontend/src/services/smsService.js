b51d585bfedcf7eeab107b1d546572353a37ead7
Yarin Cohen <yarinco123@gmail.com>
2024-12-16 01:08:22 +0200

    send sms from front

---
+++ frontend/src/services/smsService.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import config from '../config.js';
+
+const BASE_URL = config.API_BASE_URL;
+console.log(BASE_URL);
+
+export const sendSms = async (to, body) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/send-sms`, { to, body });
+    return response.data;
+  } catch (error) {
+    console.error('Failed to send SMS:', error);
+    throw error;
+  }
+};
