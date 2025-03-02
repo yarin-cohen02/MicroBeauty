804f1b1956f7366a7dc8c6aac68891f4da9360b3
Yarin Cohen <yarinco123@gmail.com>
2025-03-02 21:20:12 +0200

    update aliases of cudtomers report

--- backend/services/reportService.js
+++ backend/services/reportService.js
@@ -5,19 +5,19 @@ exports.getCustomersReportData = async () => {
   const query = `
     SELECT
         customer_id,
-        first_name AS "שם פרטי",
-        last_name AS "שם משפחה",
-        LPAD(CAST(israeli_id AS TEXT), 9, '0') AS "תעודת זהות",
-        TO_CHAR(date_of_birth,'DD/MM/YYYY') AS "תאריך לידה",
+        first_name,
+        last_name,
+        LPAD(CAST(israeli_id AS TEXT), 9, '0') AS "israeli_id",
+        TO_CHAR(date_of_birth,'DD/MM/YYYY') AS "date_of_birth",
         EXTRACT(YEAR FROM AGE(CURRENT_DATE, date_of_birth)) || '.' ||
-        LPAD(EXTRACT(MONTH FROM AGE(CURRENT_DATE, date_of_birth))::TEXT, 2, '0') AS "גיל",
-        CONCAT(SUBSTRING(mobile_number FROM 1 FOR 3), '-', SUBSTRING(mobile_number FROM 4)) AS "טלפון נייד",
-        cities.city_name AS "עיר מגורים",
-        needle_and_color AS "מחט וצבע",
-        sources.source_name AS "מקור הגעה",
-        CASE WHEN is_black_list THEN 'V' ELSE 'X' END AS "רשימה שחורה",
-        CASE WHEN agreed_ads THEN 'V' ELSE 'X' END AS "הסכמה לדיוור",
-        notes AS "הערות חופשיות"
+        LPAD(EXTRACT(MONTH FROM AGE(CURRENT_DATE, date_of_birth))::TEXT, 2, '0') AS "age",
+        CONCAT(SUBSTRING(mobile_number FROM 1 FOR 3), '-', SUBSTRING(mobile_number FROM 4)) AS "mobile_number",
+        cities.city_name AS "city_name",
+        needle_and_color,
+        sources.source_name AS "source_name",
+        is_black_list,
+        agreed_ads,
+        notes
     FROM customers
     LEFT JOIN cities ON customers.city_id = cities.city_id
     LEFT JOIN sources ON customers.source_id = sources.source_id
